bbbd5a3f9b728f4f8311f26e5149428bfffe49f9
devaduttjrofficial-sys <devaduttjrofficial@gmail.com>
2026-05-23 13:13:16 +0530

    Create Web.py

---
+++ bot/Web.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))

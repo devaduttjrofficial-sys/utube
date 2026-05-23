76b79297345eda09f803c77864bcef4f1067c598
devaduttjrofficial-sys <devaduttjrofficial@gmail.com>
2026-05-23 13:15:48 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,11 +1,22 @@
 import logging
+import threading
+import os
 
 from .utubebot import UtubeBot
 from .config import Config
 
 
+def run_web():
+    os.system("python web.py")
+
+
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG if Config.DEBUG else logging.INFO)
+    threading.Thread(target=run_web).start()
+
+    logging.basicConfig(
+        level=logging.DEBUG if Config.DEBUG else logging.INFO
+    )
+
     logging.getLogger("pyrogram").setLevel(
         logging.INFO if Config.DEBUG else logging.WARNING
     )
